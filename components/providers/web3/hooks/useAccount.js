da98019bf57d0f4ffe33c8a8c0dc09a0ee0acbca
Vinay Sawant <vinay10949@gmail.com>
2022-02-06 02:05:52 +0530

    Add files via upload

---
+++ components/providers/web3/hooks/useAccount.js
@@ -0,0 +1,43 @@
+
+
+import { useEffect } from "react"
+import useSWR from "swr"
+
+const adminAddresses = {
+  "0x8a7c30d3145786519dfab52b90a89471293df9d7176748b978381a4e9dd5f72e": true
+}
+
+export const handler = (web3, provider) => () => {
+
+  const { data, mutate, ...rest } = useSWR(() =>
+    web3 ? "web3/accounts" : null,
+    async () => {
+      const accounts = await web3.eth.getAccounts()
+      const account = accounts[0]
+
+      if (!account) {
+        throw new Error("Cannot retreive an account. Please refresh the browser.")
+      }
+      console.log("data ",web3.utils.keccak256(account))
+      return account
+    }
+  )
+
+  useEffect(() => {
+    const mutator = accounts => mutate(accounts[0] ?? null)
+    provider?.on("accountsChanged", mutator)
+
+    return () => {
+      provider?.removeListener("accountsChanged", mutator)
+    }
+  }, [provider])
+
+  return {
+    data,
+    isAdmin: (
+      data &&
+      adminAddresses[web3.utils.keccak256(data)]) ?? false,
+    mutate,
+    ...rest
+  }
+}
